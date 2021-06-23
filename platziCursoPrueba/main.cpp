23d6512421827bc76de2c096144f9c3bba5d1362
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-06-23 14:21:49 -0300

    Initial Commit

---
+++ platziCursoPrueba/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  platziCursoPrueba
+//
+//  Created by LILIA MARIANGEL on 06/23/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    int number = 2;
+    float floatA = 1.1;
+    double doubleA = 1.2;
+    char letterA = 'A';
+    
+    printf("El valor del número entero A es: %i \n", number );
+    printf("El valor de mi flotante A es: %f \n", floatA);
+    printf("El valor de mi double A es: %f \n", doubleA);
+    printf("El valor de mi caracter A es: %c \n", letterA);
+    
+    return 0;
+}
