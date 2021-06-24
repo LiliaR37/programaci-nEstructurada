814f33fc323d4ceb6fcb7693c00c19d069e909be
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-06-24 14:37:59 -0300

    operadores

---
+++ platziCursoPrueba/operadores.c
@@ -0,0 +1,41 @@
+//
+//  operadores.c
+//  platziCursoPrueba
+//
+//  Created by LILIA MARIANGEL on 06/24/21.
+//
+
+#include <stdio.h>
+
+int main() {
+    int rSum;
+    float rRes;
+    float rDiv;
+    float rMult;
+    int rMod;
+    
+    rSum = 7 + 3;
+    rRes = 7.1 - 3.1;
+    rDiv = 7.0 / 3.0;
+    rMult = 7 * 3;
+    rMod = 7 % 3;
+    
+    printf("Resultado de la suma: %i \n", rSum);
+    printf("Resultado de la resta: %f \n", rRes);
+    printf("Resultado de la división: %f \n", rDiv);
+    printf("Resultado de la multiplicación: %i \f", rMult);
+    printf("Resultado del modulo: %i \n", rMod);
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+    
+}
