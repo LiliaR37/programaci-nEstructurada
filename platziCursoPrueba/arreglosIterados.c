4bc8292c7e09c958775f99b879e8676f9310334d
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-06-29 16:10:56 -0300

    arreglos iterados

---
+++ platziCursoPrueba/arreglosIterados.c
@@ -0,0 +1,18 @@
+//
+//  arreglosIterados.c
+
+
+#include <stdio.h>
+
+int main(){
+    
+    printf("Arreglos e iteradores \n");
+    
+    int integerArray[11];
+    
+    for(int i = 0; i < 11; i++)
+    {
+        integerArray[i] = i*i;
+        printf("Valor (%i): %i \n", i, integerArray[i]);
+    }
+}
